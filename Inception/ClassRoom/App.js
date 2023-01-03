aedae1f5029f9120e9c025bcbc31bf23b11de05b
Sarvjeet Rajvansh <rajvanshsarvjeet@gmail.com>
2023-01-03 22:41:10 +0530

    Day4 : Laying Foundation

--- Inception/ClassRoom/App.js
+++ Inception/ClassRoom/App.js
@@ -1,7 +1,7 @@
-const heading1 = React.createElement("h1", { id: "head1" }, "Head1");
-const heading2 = React.createElement("h2", { id: "head2" }, "Head2");
+const heading1 = React.createElement("h1", { key: "head1" }, "Head1");
+const heading2 = React.createElement("h2", { key: "head2" }, "Head2");
 
-const container = React.createElement("div", { id: "container" }, [
+const container = React.createElement(React.Fragment, { key: "container" }, [
   heading1,
   heading2,
 ]);
