ff0b77929da02d7b6e1831bbbff76ff5d2bbea9a
Sarvjeet Rajvansh <rajvanshsarvjeet@gmail.com>
2023-01-26 01:59:11 +0530

    Adding nested routingh feature

--- showmethecode/foodvilla/src/components/Error.js
+++ showmethecode/foodvilla/src/components/Error.js
@@ -1,10 +1,21 @@
+import Header from "./Header";
+import Footer from "./Footer";
 import { useRouteError } from "react-router-dom";
+import Errimg from "../../assets/error.jpeg";
 const Error = () => {
   const err = useRouteError();
   return (
     <>
-      <h1>Page Fatt Gaya ☹️ </h1>
-      <h2>{err.status + ":" + err.message} </h2>
+      <Header />
+      <div className="msg-brd">
+        {/* <h2>{err.status + ":" + err.message} </h2> */}
+        <img
+          className="error-img"
+          src={Errimg}
+          alt="{err.status + ':' + err.message}"
+        />
+      </div>
+      <Footer />
     </>
   );
 };
